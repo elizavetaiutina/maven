7d5f0edb333274412b6f05078b037a34fe1f041d
liza <iutinaelizaveta@yandex.ru>
2025-04-02 21:19:30 +0300

    task2_array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,16 @@ package org.example;
 
 public class Main {
     public static void main(String args[]) {
-        Product ptoductOne = new Product("Футболка", "02.04.2024", "Текстильная фабрика", "Китай", 1000, true);
-        ptoductOne.getInfo();
+        Product[] productsArray = new Product[5];
+        productsArray[0]=new Product("Футболка", "02.04.2024", "Текстильная фабрика", "Китай", 1000, true);
+        productsArray[1]=new Product("Толстовка", "02.05.2023", "Текстильная фабрика", "Китай", 3000, true);
+        productsArray[2]=new Product("Штаны", "05.04.2021", "Текстильная фабрика", "Франция", 2000, false);
+        productsArray[3]=new Product("Кепка", "02.08.2023", "Головные уборы", "Китай", 700, true);
+        productsArray[4]=new Product("Очки", "02.07.2024", "Оптика", "Россия", 1000, false);
+        productsArray[0].getInfo();
+        productsArray[1].getInfo();
+        productsArray[2].getInfo();
+        productsArray[3].getInfo();
+        productsArray[4].getInfo();
     }
 }
