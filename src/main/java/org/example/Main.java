7a03eca873440610d371986383980ea3ba9e98ce
liza <iutinaelizaveta@yandex.ru>
2025-04-02 20:44:40 +0300

    task1_classProduct

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String args[]) {
+        Product ptoductOne = new Product("Футболка", "02.04.2024", "Текстильная фабрика", "Китай", 1000, true);
+        ptoductOne.getInfo();
     }
 }
