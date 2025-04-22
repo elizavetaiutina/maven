7640639d90a9898c12806251ec0115ababa0f7a6
lizaiutina <iutinaelizaveta@yandex.ru>
2025-04-22 14:38:43 +0300

    task2 chek data array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,39 @@
 package org.example;
-//import java.util.Arrays;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) {
-        String[][] array = new String[4][4];
+        String[][] array = new String[][]{{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}};
         try {
-            check(array);
-            System.out.println("Двумерный массив имеет размер 4х4");
-        } catch (MyArraySizeException e) {
-            System.out.println("Массив имеет другой размер !");
+            try {
+                check(array);
+                System.out.println("Двумерный массив имеет размер 4х4");
+            } catch (MyArraySizeException e) {
+                System.out.println("Массив имеет другой размер !");
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("В ячейке " + "[" + e.i + "]" + "[" + e.j + "]" + " лежат неверные данные");
         }
     }
 
-    public static void check(String[][] arr) throws MyArraySizeException {
+    public static void check(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         if (arr.length != 4) {
             throw new MyArraySizeException();
         }
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].length != 4) {
                 throw new MyArraySizeException();
             }
+            for (int j = 0; j < arr.length; j++) {
+                try {
+                    sum += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
         }
+        System.out.println("Сумма значений в ячейках = " + sum);
     }
 }
