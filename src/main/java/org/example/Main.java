0bb2981bb78ab14360b8767a0e212c5f4082e3af
Elizaveta Iutina <112534801+elizavetaiutina@users.noreply.github.com>
2025-05-04 13:57:55 +0300

    Lesson 7 junit 5 (#7)
    * init branch
    
    * 4test for task
    
    * Delete src/main/java/org/example/Animal.java
    
    * Delete src/main/java/org/example/Cat.java
    
    * Delete src/main/java/org/example/Circle.java
    
    * Delete src/main/java/org/example/Dish.java
    
    * Delete src/main/java/org/example/Dog.java
    
    * Delete src/main/java/org/example/Figure.java
    
    * Delete src/main/java/org/example/MyArrayDataException.java
    
    * Delete src/main/java/org/example/MyArraySizeException.java
    
    * Delete src/main/java/org/example/Rectangle.java
    
    * Delete src/main/java/org/example/Triangle.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,39 +1,30 @@
 package org.example;
 
-import java.util.*;
-
 public class Main {
     public static void main(String args[]) {
-        Set<Student> students = new HashSet<>();
-        Student studentOne = new Student("Андрей", "E200E", 3, Arrays.asList(3, 4, 5, 4));
-        Student studentTwo = new Student("Петр", "E200E", 3, Arrays.asList(2, 3, 3, 3));
-        Student student3 = new Student("Иван", "E200E", 3, Arrays.asList(5, 5, 5, 5));
-        Student student4 = new Student("Алексей", "E200E", 1, Arrays.asList(2, 2, 2, 2));
-        Student student5 = new Student("Павел", "E200E", 2, Arrays.asList(2, 2, 2, 2));
-        Student student6 = new Student("Максим", "E200E", 2, Arrays.asList(3, 4, 3, 3));
-        students.add(studentOne);
-        students.add(studentTwo);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
-        students.add(student6);
-        System.out.println("Изначальный список студентов: " + students + "\n");
-
-        Methods.expulsiontStudents(students);
-        Methods.switchToNextCourseStudent(students);
-        Methods.printStudents(students, 3);
-        System.out.println("\nТЕПЕРЬ ТАК : " + students);
-
-        System.out.println("\n\nЗАДАНИЕ 2");
-        Phonebook phonebook = new Phonebook();
-
-        phonebook.addNumberPhone("Иванов", "+7999999999");
-        phonebook.addNumberPhone("Петров", "+7111111111");
-        phonebook.addNumberPhone("Петров", "+7222222222");
-
-        phonebook.getPhoneBook();
-        phonebook.getNumberPhone("Иванов");
-        phonebook.getNumberPhone("Петров");
-        phonebook.getNumberPhone("Иутина");//несуществующая фамилия
     }
+
+    public static int factorial(int a) {
+        int b = 1;
+        for (int i = 1; i <= a; i++) {
+            b *= i;
+        }
+        return b;
+    }
+
+    public static double squareTriangle(int a, int b, int c) {
+        double polupreimetr = (double) (a + b + c) / 2;
+        return Math.sqrt(polupreimetr * (polupreimetr - a) * (polupreimetr - b) * (polupreimetr - c));
+    }
+
+    public static String comparison(int a, int b) {
+        if (a > b) {
+            return ">";
+        } else if (a < b) {
+            return "<";
+        } else {
+            return "=";
+        }
+    }
+
 }
