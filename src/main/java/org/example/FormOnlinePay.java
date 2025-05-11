9688664d8726d50bc1863ccdd273ba0d48d0f149
lizaiutina <iutinaelizaveta@yandex.ru>
2025-05-11 15:53:34 +0300

    тесты для проверки отображения данных фрейма

--- src/main/java/org/example/FormOnlinePay.java
+++ src/main/java/org/example/FormOnlinePay.java
@@ -1,19 +1,26 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 
 public class FormOnlinePay {
+    WebDriverWait wait;
     WebDriver driver;
 
     public FormOnlinePay(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(3));
     }
 
     public String getTitleBlock() {
+        acceptCookiesIfPresent();
         return driver.findElement(By.xpath("//section[@class='pay']//h2")).getText();
     }
 
@@ -27,7 +34,8 @@ public class FormOnlinePay {
         return driver.getCurrentUrl();
     }
 
-    public void checkOfButton() {
+    public FormForPayWithCard checkOfButton() {
+        acceptCookiesIfPresent();
         WebElement phone = driver.findElement(By.id("connection-phone"));
         phone.sendKeys("297777777");
 
@@ -36,9 +44,20 @@ public class FormOnlinePay {
 
         WebElement buttonAdd = driver.findElement(By.xpath("//form[@id='pay-connection']/button"));
         buttonAdd.click();
+
+        return new FormForPayWithCard(driver);
     }
 
     public String getPlaceHolder(String id) {
         return driver.findElement(By.id(id)).getAttribute("placeholder");
+    }
+
+    protected void acceptCookiesIfPresent() {
+        try {
+            WebElement acceptCookie = wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//button[contains(., 'Принять')]")));
+            acceptCookie.click();
+        } catch (TimeoutException ignored) {
+        }
     }
 }
